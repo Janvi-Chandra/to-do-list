0915655671a708f85f81d4087b84c657bd53f27f
Janvi Chandra <janvichandra4@gmail.com>
2024-07-02 11:19:55 +0530

    Login Page UI Finished

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TodoWrapper from './components/TodoWrapper';
 import Home from './components/Home';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/todolist" element={<TodoWrapper />} />
+          <Route exact path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
